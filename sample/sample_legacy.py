f4b760ed37bec1df2131e47855e2ae48a98a2eef
Y.Takano <ytakano@weathermap.co.jp>
2022-10-05 09:56:17 +0900

    support multiprocessing in sample

--- sample/sample_legacy.py
+++ sample/sample_legacy.py
@@ -1,3 +1,8 @@
+import time
+import argparse
+
+import multiprocessing
+import functools
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
@@ -7,23 +12,64 @@ from nicopy.grids import LegacyGrids
 from nicopy.reader import LegacyReader
 from nicopy.util import calc_lall
 
-# create fig and ax
-projection = ccrs.Orthographic(central_latitude=0.0, central_longitude=0.0)
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection=projection)
 
-glevel = 5
-rlevel = 1
-lall = calc_lall(rlevel)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mp", type=int, help="use multiprocessing")
+    args = parser.parse_args()
 
-for l in range(lall):
-    # read grids
+    glevel = 5
+    rlevel = 1
+    lall = calc_lall(rlevel)
+
+    start_time = time.perf_counter()
+    if args.mp:
+        with multiprocessing.Pool(processes=args.mp) as pool:
+            data = pool.map(
+                functools.partial(read_data, glevel, rlevel),
+                range(lall),
+            )
+    else:
+        data = []
+        for l in range(lall):
+            data.append(read_data(glevel, rlevel, l))
+    end_time = time.perf_counter()
+    print("read:", end_time - start_time)
+
+    start_time = time.perf_counter()
+    projection = ccrs.Orthographic(central_latitude=0.0, central_longitude=0.0)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection=projection)
+
+    cmap = plt.cm.jet
+    norm = mcolors.Normalize(vmin=0, vmax=30)
+
+    for lonlat_v, sa_t2m in data:
+        ncells, _, _ = lonlat_v.shape
+        gscolors = cmap(norm(sa_t2m))
+        for i in range(ncells):
+            poly = plt.Polygon(lonlat_v[i], fc=gscolors[i], transform=ccrs.Geodetic())
+            ax.add_patch(poly)
+
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    fig.colorbar(sm, ax=ax)
+
+    ax.set_global()
+    ax.coastlines()
+    ax.gridlines()
+
+    plt.savefig("sample_legacy.png")
+    plt.clf()
+    plt.close()
+    end_time = time.perf_counter()
+    print("draw:", end_time - start_time)
+
+
+def read_data(glevel: int, rlevel: int, l: int):
     grids = LegacyGrids(glevel, rlevel, f"./testdata/grid/grid.rgn{l:05d}")
     lon_v, lat_v = grids.get_lonlat_v()
     lonlat_v = np.stack([lon_v, lat_v], axis=-1)
-    ncells, nv, _ = lonlat_v.shape
 
-    # read data
     kall = 1
     reader = LegacyReader(
         glevel, rlevel, kall, f"./testdata/data_legacy/sa_t2m.rgn{l:05d}"
@@ -31,22 +77,8 @@ for l in range(lall):
     step = 0
     k = 0
     sa_t2m = reader.read_rgn(step, k) - 273.15
+    return lonlat_v, sa_t2m
 
-    # draw grid
-    cmap = plt.cm.jet
-    norm = mcolors.Normalize(vmin=0, vmax=30)
-    gscolors = [cmap(norm(r)) for r in sa_t2m]
-    for i in range(ncells):
-        poly = plt.Polygon(lonlat_v[i], fc=gscolors[i], transform=ccrs.Geodetic())
-        ax.add_patch(poly)
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
 
-fig.colorbar(sm, ax=ax)  # plot colorbar
-ax.set_global()
-ax.coastlines()
-ax.gridlines()
-
-# save and close
-plt.savefig("sample_legacy.png")
-plt.clf()
-plt.close()
+if __name__ == "__main__":
+    main()
--- sample/sample_panda.py
+++ sample/sample_panda.py
@@ -1,3 +1,8 @@
+import time
+import argparse
+
+import multiprocessing
+import functools
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
@@ -7,48 +12,77 @@ from nicopy.grids import LegacyGrids
 from nicopy.reader import PandaReader
 from nicopy.util import calc_lall
 
-# create fig and ax
-projection = ccrs.Orthographic(central_latitude=0.0, central_longitude=0.0)
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection=projection)
 
-glevel = 5
-rlevel = 1
-run_pe = 8
-lall = calc_lall(rlevel)
-num_of_rgn = int(lall / run_pe)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mp", type=int, help="use multiprocessing")
+    args = parser.parse_args()
 
-for pe in range(run_pe):
-    # read data
-    kall = 1
+    glevel = 5
+    rlevel = 1
+    run_pe = 8
+    lall = calc_lall(rlevel)
+    num_of_rgn = int(lall / run_pe)
+
+    start_time = time.perf_counter()
+    if args.mp:
+        with multiprocessing.Pool(processes=args.mp) as pool:
+            r = pool.map(
+                functools.partial(read_data, glevel, rlevel, num_of_rgn),
+                range(run_pe),
+            )
+        data = sum(r, [])
+    else:
+        data = []
+        for pe in range(run_pe):
+            data.extend(read_data(glevel, rlevel, num_of_rgn, pe))
+    end_time = time.perf_counter()
+    print("read:", end_time - start_time)
+
+    start_time = time.perf_counter()
+    projection = ccrs.Orthographic(central_latitude=0.0, central_longitude=0.0)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection=projection)
+
+    cmap = plt.cm.jet
+    norm = mcolors.Normalize(vmin=100, vmax=300)
+
+    for lonlat_v, sa_lwu_toa in data:
+        ncells, _, _ = lonlat_v.shape
+        gscolors = cmap(norm(sa_lwu_toa))
+        for i in range(ncells):
+            poly = plt.Polygon(lonlat_v[i], fc=gscolors[i], transform=ccrs.Geodetic())
+            ax.add_patch(poly)
+
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    fig.colorbar(sm, ax=ax)
+    ax.set_global()
+    ax.coastlines()
+    ax.gridlines()
+
+    plt.savefig("sample_panda.png")
+    plt.clf()
+    plt.close()
+
+    end_time = time.perf_counter()
+    print("draw:", end_time - start_time)
+
+
+def read_data(glevel: int, rlevel: int, num_of_rgn: int, pe: int):
     reader = PandaReader(f"./testdata/data_panda_8PE/history.pe{pe:06d}")
     step = 1
     k = 0
     sa_lwu_toa = reader.read_pe("sa_lwu_toa", step, k)
 
+    output = []
     for rgn in range(num_of_rgn):
         # read grids
         l = num_of_rgn * pe + rgn
         grids = LegacyGrids(glevel, rlevel, f"./testdata/grid/grid.rgn{l:05d}")
         lon_v, lat_v = grids.get_lonlat_v()
         lonlat_v = np.stack([lon_v, lat_v], axis=-1)
-        ncells, nv, _ = lonlat_v.shape
+        output.append((lonlat_v, sa_lwu_toa[rgn]))
 
-        # draw grid
-        cmap = plt.cm.jet
-        norm = mcolors.Normalize(vmin=100, vmax=300)
-        gscolors = [cmap(norm(r)) for r in sa_lwu_toa[rgn, :]]
-        for i in range(ncells):
-            poly = plt.Polygon(lonlat_v[i], fc=gscolors[i], transform=ccrs.Geodetic())
-            ax.add_patch(poly)
-        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
 
-fig.colorbar(sm, ax=ax)  # plot colorbar
-ax.set_global()
-ax.coastlines()
-ax.gridlines()
-
-# save and close
-plt.savefig("sample_panda.png")
-plt.clf()
-plt.close()
+if __name__ == "__main__":
+    main()
