ccb23ded52fae80cb0b91eb30706b107343c463c
Y.Takano <ytakano@weathermap.co.jp>
2022-10-05 13:54:46 +0900

    sample with datashader

--- sample/sample.py
+++ sample/sample.py
@@ -1,32 +1,43 @@
 import time
 import argparse
+import warnings
 
 import multiprocessing
 import functools
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+from matplotlib.cm import jet
 import cartopy.crs as ccrs
+from spatialpandas import GeoDataFrame
+from spatialpandas.geometry import PolygonArray
+import datashader as ds
+import datashader.transfer_functions as tf
 
 from nicopy.grids import LegacyGrids
 from nicopy.reader import LegacyReader, PandaReader
 from nicopy.util import calc_lall
 
+warnings.simplefilter("ignore", matplotlib.MatplotlibDeprecationWarning)
+
 
 def main():
+    # get args
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--format", default="legacy", help="ico format (legacy | panda)"
     )
     parser.add_argument("--mp", type=int, help="use multiprocessing")
+    parser.add_argument("--plot", default="mpl", help="plot library (mpl | ds)")
     args = parser.parse_args()
 
+    # general settings
     glevel = 5
     rlevel = 1
-    run_pe = 8
     lall = calc_lall(rlevel)
-    num_of_rgn = int(lall / run_pe)
 
+    # read data
     start_time = time.perf_counter()
     if args.format == "legacy":
         if args.mp:
@@ -40,6 +51,8 @@ def main():
             for l in range(lall):
                 data.append(read_legacy(glevel, rlevel, l))
     if args.format == "panda":
+        run_pe = 8
+        num_of_rgn = int(lall / run_pe)
         if args.mp:
             with multiprocessing.Pool(processes=args.mp) as pool:
                 r = pool.map(
@@ -54,31 +67,81 @@ def main():
     end_time = time.perf_counter()
     print("read:", end_time - start_time)
 
+    # plot figure
     start_time = time.perf_counter()
     projection = ccrs.Orthographic(central_latitude=0.0, central_longitude=0.0)
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection=projection)
+    if args.plot == "ds":
+        outlines = []
+        _values = []
+        for lonlat_v, sa_t2m in data:
+            ncells, _, _ = lonlat_v.shape
+            for i in range(ncells):
+                coords = projection.transform_points(
+                    ccrs.Geodetic(), lonlat_v[i, :, 0], lonlat_v[i, :, 1]
+                )
+                points = coords[:, 0:2].ravel()
+                outline = np.concatenate([points, points[0:2]])
+                outlines.append([outline])
+            _values.append(sa_t2m)
+        values = np.concatenate(_values)
+        polygons = PolygonArray(outlines)
+        df = GeoDataFrame({"polygons": polygons, "values": values})
 
-    cmap = plt.cm.jet
-    norm = mcolors.Normalize(vmin=0, vmax=30)
+        vmin = 0
+        vmax = 30
+        cmap = jet
+        cvs = ds.Canvas()
+        agg = cvs.polygons(df, geometry="polygons", agg=ds.last("values"))
+        shade = tf.shade(agg, cmap=cmap, how="linear", span=[vmin, vmax])
 
-    for lonlat_v, sa_t2m in data:
-        ncells, _, _ = lonlat_v.shape
-        gscolors = cmap(norm(sa_t2m))
-        for i in range(ncells):
-            poly = plt.Polygon(lonlat_v[i], fc=gscolors[i], transform=ccrs.Geodetic())
-            ax.add_patch(poly)
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection=projection)
 
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-    fig.colorbar(sm, ax=ax)
+        left = projection.transform_point(-90, 0, ccrs.Geodetic())[0]
+        right = projection.transform_point(90, 0, ccrs.Geodetic())[0]
+        bottom = projection.transform_point(0, -90, ccrs.Geodetic())[1]
+        top = projection.transform_point(0, 90, ccrs.Geodetic())[1]
+        ax.imshow(shade.to_pil(), extent=(left, right, bottom, top))
 
-    ax.set_global()
-    ax.coastlines()
-    ax.gridlines()
+        norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        fig.colorbar(sm, ax=ax)
 
-    plt.savefig(f"sample_{args.format}.png")
-    plt.clf()
-    plt.close()
+        ax.set_global()
+        ax.coastlines()
+        ax.gridlines()
+
+        ax.set_title("matplotlib + datashader (imshow)")
+        plt.savefig(f"sample_ds_{args.format}.png")
+        plt.clf()
+        plt.close()
+    elif args.plot == "mpl":
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection=projection)
+
+        cmap = plt.cm.jet
+        norm = mcolors.Normalize(vmin=0, vmax=30)
+
+        for lonlat_v, sa_t2m in data:
+            ncells, _, _ = lonlat_v.shape
+            gscolors = cmap(norm(sa_t2m))
+            for i in range(ncells):
+                poly = plt.Polygon(
+                    lonlat_v[i], fc=gscolors[i], transform=ccrs.Geodetic()
+                )
+                ax.add_patch(poly)
+
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        fig.colorbar(sm, ax=ax)
+
+        ax.set_global()
+        ax.coastlines()
+        ax.gridlines()
+
+        ax.set_title("matplotlib add_patch")
+        plt.savefig(f"sample_mpl_{args.format}.png")
+        plt.clf()
+        plt.close()
     end_time = time.perf_counter()
     print("draw:", end_time - start_time)
 
