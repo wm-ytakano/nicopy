bb3d734f3394b298111bce4d285531bfa780cece
Y.Takano <ytakano@weathermap.co.jp>
2023-09-30 20:03:31 +0900

    Add options to LegacyReader

--- nicopy/reader/LegacyReader.py
+++ nicopy/reader/LegacyReader.py
@@ -11,15 +11,47 @@ class LegacyReader:
         self.kall = kall
         self.filename = filename
 
-    def read_rgn(self, step: int, k: int):
+    def read_rgn(
+        self,
+        step: int,
+        k: int,
+        precision: int = 4,
+        output_halo: bool = False,
+        output_shape: str = "2D",
+        access: str = "direct",
+    ):
+        if output_shape not in ["1D", "2D"]:
+            raise ValueError("'output_shape' must be '1D' or '2d'")
+        if access not in ["direct", "sequential"]:
+            raise ValueError("'access' must be 'direct' or 'sequential'")
         gall = self.gall
         kall = self.kall
         f: typing.BinaryIO = open(self.filename, "rb")
-        offset = step * kall * gall * 4
-        shape = (kall, gall)
-        v_all = np.memmap(f, mode="r", dtype=">f4", offset=offset, shape=shape)[k, :]
+        if access == "sequential":
+            offset = 4
+        else:
+            offset = 0
+        v_all = np.memmap(
+            f,
+            mode="r",
+            dtype=f">f{precision}",
+            offset=step * kall * gall * precision + offset,
+            shape=(kall, gall),
+        )[k, :]
         gall_1d = self.gall_1d
-        return v_all.reshape((gall_1d, gall_1d))[
-            1 : gall_1d - 1,
-            1 : gall_1d - 1,
-        ].ravel()
+        if output_halo:
+            if output_shape == "1D":
+                return v_all
+            else:
+                return v_all.reshape((gall_1d, gall_1d))
+        else:
+            if output_shape == "1D":
+                return v_all.reshape((gall_1d, gall_1d))[
+                    1 : gall_1d - 1,
+                    1 : gall_1d - 1,
+                ].ravel()
+            else:
+                return v_all.reshape((gall_1d, gall_1d))[
+                    1 : gall_1d - 1,
+                    1 : gall_1d - 1,
+                ]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nicopy",
-    version="0.0.1",
+    version="0.0.2",
     description="NICAM Icosahedral Grid Operator for Python",
     license="BSD-3-Clause",
     packages=find_packages(),
